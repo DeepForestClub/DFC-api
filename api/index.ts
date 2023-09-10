1ab419fbc6c44f3a6c18c4651148246ce8664f80
lest-day <106459514+lest-day@users.noreply.github.com>
2023-09-10 11:49:11 +0800

    修改

--- api/index.ts
+++ api/index.ts
@@ -1,40 +1,60 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-const rateLimit = (handler: (req: VercelRequest, res: VercelResponse) => void, limit: number, interval: number) => {
-  const requests = new Map<string, number>();
+interface RequestRecord {
+  count: number;
+  timeout: NodeJS.Timeout | null;
+}
+
+const rateLimit = (
+  handler: (req: VercelRequest, res: VercelResponse) => void,
+  limit: number,
+  interval: number
+) => {
+  const requests = new Map<string, RequestRecord>();
 
   return async (request: VercelRequest, response: VercelResponse) => {
-    // Get IP address of client by checking for x-forwarded-for header, which may
-    // contain multiple IP addresses if there are intermediate proxies.
-    const ip = request.headers['x-forwarded-for']?.split(',')[0].trim() || request.socket.remoteAddress;
+    const ip =
+      request.headers['x-forwarded-for']?.split(',')[0].trim() ||
+      request.socket?.remoteAddress;
 
-    // Check if a token is provided
+    if (!ip) {
+      return response.status(400).json({ message: 'Invalid Request' });
+    }
+
     const token = request.query.token || '';
     const secretToken = process.env.SECRET_TOKEN;
 
-    // Reject the request if no token is provided
     if (!secretToken && !token) {
       return response.status(401).json({ message: 'Access Denied' });
     }
 
-    // Allow requests without rate limit if the correct token is provided
     if (secretToken && token === secretToken) {
       return handler(request, response);
     }
 
-    // Check if the client has exceeded the rate limit
-    const numRequests = requests.get(ip) || 0;
-    if (numRequests >= limit) {
+    let record = requests.get(ip);
+
+    if (!record) {
+      record = { count: 1, timeout: null };
+      requests.set(ip, record);
+    } else {
+      record.count++;
+    }
+
+    if (record.count > limit) {
+      if (record.timeout) {
+        clearTimeout(record.timeout);
+      }
+
       return response.status(429).json({ message: 'Too Many Requests' });
     }
 
-    // Increment the count of requests made by the client
-    requests.set(ip, numRequests + 1);
+    if (!record.timeout) {
+      record.timeout = setTimeout(() => {
+        requests.delete(ip);
+      }, interval);
+    }
 
-    // Reset the count after the specified interval
-    setTimeout(() => requests.delete(ip), interval);
-
-    // Call the original handler
     return handler(request, response);
   };
 };
