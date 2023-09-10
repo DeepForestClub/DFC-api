f50b1b4aea799f7d134a85c0b10d02291dc5c68c
lest-day <106459514+lest-day@users.noreply.github.com>
2023-09-10 11:17:25 +0800

    第一次测试性提交

---
+++ api/index.ts
@@ -0,0 +1,36 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+const rateLimit = (handler: (req: VercelRequest, res: VercelResponse) => void, limit: number, interval: number) => {
+  const requests = new Map<string, number>();
+
+  return async (request: VercelRequest, response: VercelResponse) => {
+    const ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress;
+
+    if (!requests.has(ip)) {
+      requests.set(ip, 1);
+    } else {
+      if (requests.get(ip)! >= limit) {
+        return response.status(429).json({ message: 'Too Many Requests' });
+      }
+      requests.set(ip, requests.get(ip)! + 1);
+    }
+
+    setTimeout(() => {
+      requests.delete(ip);
+    }, interval);
+
+    return handler(request, response);
+  };
+};
+
+const apiHandler = (request: VercelRequest, response: VercelResponse) => {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
+};
+
+const handlerWithRateLimit = rateLimit(apiHandler, 10, 60000);
+
+export default handlerWithRateLimit;
