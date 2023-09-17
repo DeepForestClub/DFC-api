b21d41128d0292a3c7a1ab7ccf876b35edd1c700
lest-day <106459514+lest-day@users.noreply.github.com>
2023-09-17 08:26:50 +0800

    添加功能

--- api/statistics/index.ts
+++ api/statistics/index.ts
@@ -61,45 +61,74 @@ const rateLimit = (
   };
 };
 
-const getPageTags = async (url: string): Promise<number> => {
+const getTagNumber = async (url: string, tagSelector: string): Promise<number> => {
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-    const tagNumber = $('.pages-list-item').length;
-    console.log(`TagNumber: ${tagNumber}`);
+    const tagNumber = $(tagSelector).length;
     return tagNumber;
   } catch (error) {
     console.error(error);
-    return -1; // 返回一个错误标识值
+    return -1;
   }
 };
 
-const apiHandler = async (request: VercelRequest, response: VercelResponse, url: string, propertyName: string) => {
-  const tagNumber = await getPageTags(url);
+const apiHandler = async (
+  request: VercelRequest,
+  response: VercelResponse,
+  url: string,
+  propertyName: string,
+  tagSelector: string
+) => {
+  const tagNumber = await getTagNumber(url, tagSelector);
   const responseData = {
     [propertyName]: tagNumber,
   };
   response.status(200).json(responseData);
 };
 
-const handlerWithRateLimit = rateLimit(apiHandler, 10, 60000); // 允许每分钟最多发出10个请求
+const handlerWithRateLimit = rateLimit(
+  (req: VercelRequest, res: VercelResponse) => {
+    apiHandler(req, res, 'https://deep-forest-club.wikidot.com/system:page-tags-list', 'TagNumber', '.pages-tag-cloud-box > a.tag');
+  },
+  10,
+  60000
+);
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const originalNumberPromise = getPageTags('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E5%8E%9F%E5%88%9B');
-  const reprintedNumberPromise = getPageTags('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E6%90%AC%E8%BF%90');
-  const articlesNumberPromise = getPageTags('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E6%96%87%E7%AB%A0');
-  const artNumberPromise = getPageTags('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E8%89%BA%E6%9C%AF%E4%BD%9C%E5%93%81');
+  const originalNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E5%8E%9F%E5%88%9B', '.pages-list-item');
+  const reprintedNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E6%90%AC%E8%BF%90', '.pages-list-item');
+  const articlesNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E6%96%87%E7%AB%A0', '.pages-list-item');
+  const topicArticlesNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E4%B8%BB%E9%A2%98%E6%96%87%E7%AB%A0', '.pages-list-item');
+  const nonTopicArticlesNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E9%9D%9E%E4%B8%BB%E9%A2%98%E6%96%87%E7%AB%A0', '.pages-list-item');
+  const cooperationTopicArticlesNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E5%90%88%E4%BD%9C%E4%B8%BB%E9%A2%98%E6%96%87%E7%AB%A0', '.pages-list-item');
+  const cooperationNonTopicArticlesNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E5%90%88%E4%BD%9C%E9%9D%9E%E4%B8%BB%E9%A2%98%E6%96%87%E7%AB%A0', '.pages-list-item');
+  const artNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E8%89%BA%E6%9C%AF%E4%BD%9C%E5%93%81', '.pages-list-item');
+  const spaceNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags/tag/%E6%88%90%E5%91%98%E9%A1%B5', '.pages-list-item');
+  const tagNumberPromise = getTagNumber('https://deep-forest-club.wikidot.com/system:page-tags-list', '.pages-tag-cloud-box > a.tag');
 
   const originalNumber = await originalNumberPromise;
   const reprintedNumber = await reprintedNumberPromise;
   const articlesNumber = await articlesNumberPromise;
+  const topicArticlesNumber = await topicArticlesNumberPromise;
+  const nonTopicArticlesNumber = await nonTopicArticlesNumberPromise;
+  const cooperationTopicArticlesNumber = await cooperationTopicArticlesNumberPromise;
+  const cooperationNonTopicArticlesNumber = await cooperationNonTopicArticlesNumberPromise;
   const artNumber = await artNumberPromise;
+  const spaceNumber = await spaceNumberPromise;
+  const tagNumber = await tagNumberPromise;
 
   const responseData = {
     OriginalNumber: originalNumber,
     ReprintedNumber: reprintedNumber,
     ArticlesNumber: articlesNumber,
+    topicArticlesNumber: topicArticlesNumber,
+    NonTopicArticlesNumber: nonTopicArticlesNumber,
+    CooperationTopicArticlesNumber: cooperationTopicArticlesNumber,
+    CooperationNonTopicArticlesNumber: cooperationNonTopicArticlesNumber,
     ArtNumber: artNumber,
+    SpaceNumber: spaceNumber,
+    TagNumber: tagNumber,
   };
 
   response.status(200).json(responseData);
