0a0b32386cf35b6df991d9033c762f625e8e61bd
lest-day <106459514+lest-day@users.noreply.github.com>
2023-09-24 12:15:25 +0800

    Update index.ts

--- api/pages/index.ts
+++ api/pages/index.ts
@@ -8,6 +8,7 @@ timeout: NodeJS.Timeout | null;
 }
 
 const rateLimit = (
+<<<<<<< HEAD
 handler: (req: VercelRequest, res: VercelResponse) => void,
 limit: number,
 interval: number
@@ -127,3 +128,99 @@ async (req: VercelRequest, res: VercelResponse) => {
 10,
 1000
 );
+=======
+    handler: (req: VercelRequest, res: VercelResponse) => void,
+    limit: number,
+    interval: number
+  ) => {
+    const requests = new Map<string, RequestRecord>();
+  
+    return async (request: VercelRequest, response: VercelResponse) => {
+      const ip =
+        request.headers['x-forwarded-for']?.split(',')[0].trim() ||
+        request.socket?.remoteAddress;
+  
+      if (!ip) {
+        return response.status(400).json({ message: 'Invalid Request' });
+      }
+  
+      const token = request.query.token || '';
+      const secretToken = process.env.SECRET_TOKEN;
+  
+      if (!secretToken && !token) {
+        return response.status(401).json({ message: 'Access Denied' });
+      }
+  
+      if (secretToken && token === secretToken) {
+        return handler(request, response);
+      }
+  
+      let record = requests.get(ip);
+  
+      if (!record) {
+        record = { count: 1, timeout: null };
+        requests.set(ip, record);
+      } else {
+        record.count++;
+      }
+  
+      if (record.count > limit) {
+        if (record.timeout) {
+          clearTimeout(record.timeout);
+        }
+  
+        return response.status(429).json({ message: 'Too Many Requests' });
+      }
+  
+      if (!record.timeout) {
+        record.timeout = setTimeout(() => {
+          requests.delete(ip);
+        }, interval);
+      }
+  
+      return handler(request, response);
+    };
+  };
+
+async function getPageLinks(url: string): Promise<string[]> {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    const links: string[] = [];
+    $('a').each((_, element) => {
+        const href = $(element).attr('href');
+        if (href && href.startsWith('/')) {
+            links.push(href);
+        }
+    });
+
+    return links;
+}
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+    try {
+        // 获取页面数量 Y
+        const listAllPagesUrl = 'https://deep-forest-club.wikidot.com/system:list-all-pages';
+        const listAllPagesResponse = await axios.get(listAllPagesUrl);
+        const listAllPages$ = cheerio.load(listAllPagesResponse.data);
+        const pagerText = listAllPages$('.pager-no').html();
+        const totalPages = parseInt(pagerText?.split(' of ')[1] || '0', 10);
+
+        // 访问每个页面并获取链接
+        const allLinks: string[] = [];
+        for (let i = 1; i <= totalPages; i++) {
+            const pageUrl = `${listAllPagesUrl}/p/${i}`;
+            const linksOnPage = await getPageLinks(pageUrl);
+            allLinks.push(...linksOnPage);
+        }
+
+        // 去除重复链接
+        const uniqueLinks = [...new Set(allLinks)];
+
+        res.status(200).json(uniqueLinks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+>>>>>>> parent of adc7422 (Update index.ts)
